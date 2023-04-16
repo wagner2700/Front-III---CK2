a2b9e382afaca148b684b4608787c87086330f30
Guilherme Santos <86587662+Gui-dsr@users.noreply.github.com>
2023-04-16 20:21:06 -0300

    Darkmode

--- ctd-fe3-final-base/src/Components/Navbar.jsx
+++ ctd-fe3-final-base/src/Components/Navbar.jsx
@@ -1,37 +1,51 @@
 import styles from "./Navbar.module.css";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/auth-context";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const navigate = useNavigate()
-  const{removeUserStorage} = useContext(AuthContext)
+  const navigate = useNavigate();
+  const { removeUserStorage } = useContext(AuthContext);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  useEffect(()=>{
-    const token = localStorage.getItem("@DataToken")
+  useEffect(() => {
+    const token = localStorage.getItem("@DataToken");
 
-    if(!token){
-      navigate("/login")
+    if (!token) {
+      navigate("/login");
     }
-  },[])
+  }, []);
 
-  function logout(){
-    removeUserStorage()
-    navigate("/login")
+  function logout() {
+    removeUserStorage();
+    navigate("/login");
   }
 
+  function toggleDarkMode() {
+    setIsDarkMode(!isDarkMode);
+    const body = document.querySelector('body');
+    const html = document.querySelector('html');
+    if (body) {
+      body.classList.toggle('dark');
+    }
+    if (html) {
+      html.classList.toggle('dark');
+    }
+  }
 
   return (
     <header className="sticky-top">
-      {/* //Na linha seguinte deverá ser feito um teste se a aplicação
-        // está em dark mode e deverá utilizar navbar-dark bg-dark ou navbar-light bg-light*/}
       <nav
-        className={`navbar navbar-expand-sm navbar-light bg-light`}
+        className={`navbar navbar-expand-sm ${
+          isDarkMode ? "navbar-dark bg-dark" : "navbar-light bg-light"
+        }`}
         aria-label="Third navbar example"
       >
         <div className="container">
-          {/* Ao clicar, o usuário deve ser redirecionado a home, com react-router */}
-          <a className={`navbar-brand ${styles.navbarBrand}`} href="/home">
+          <a
+            className={`navbar-brand ${styles.navbarBrand}`}
+            href="/home"
+          >
             DH Odonto
           </a>
           <button
@@ -52,33 +66,30 @@ const Navbar = () => {
           >
             <ul className="navbar-nav mb-2 mb-sm-0">
               <li className={`nav-item ${styles.navBarLink}`}>
-                {/* Ao clicar, o usuário deve ser redirecionado a home, com react-router */}
                 <a className="nav-link" href="/home">
                   Home
                 </a>
               </li>
               <li className={`nav-item ${styles.navBarLink}`}>
-                {/* Se o usuário estiver logado, deverá aparecer um botão de logout
-                que vai apagar o token do localstorage.
-                Se o usuário estiver deslogado, um link fará um redirecionamento, com react-router,
-                ao formulário de login
-                O botão de logout deverá ser testado darkmode
-                se sim, btn-dark, se não, btn-light */}
-                <button className="nav-link" href="/login" onClick={logout}>
-                  Login 
-                </button>
+                <a
+                  className={`nav-link ${
+                    isDarkMode ? "btn-dark" : "btn-light"}`}
+                  href="/login"
+                  onClick={logout}
+                >
+                  Logout
+                  </a>
               </li>
               <li className={`nav-item`}>
-                {/* Ao ser clicado, esse botão mudará a aplicação para dark mode ou light mode.
-                 Lembre-se de usar um estado no contexto para fazer essa alteração.
-                 Na linha seguinte deverá ser feito um teste se a aplicação
-                 está em dark mode e deverá utilizar o icone ☀ ou 🌙 e btn-dark ou btn-light*/}
-                <button
-                  className={`btn btn-light${styles.btnStyle
-                    }`}
-                >
-                  ☀ 🌙{" "}
-                </button>
+              <button
+                className={`btn ${
+                  isDarkMode ? "btn-light" : "btn-dark"
+                } ${styles.btnStyle}`}
+                onClick={toggleDarkMode}
+              >
+                {isDarkMode ? "☀️" : "🌙"}
+              </button>
+
               </li>
             </ul>
           </div>
