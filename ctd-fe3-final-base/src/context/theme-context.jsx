b9d638825d67876d352263a2a1e79156846cfe60
Guilherme Santos <86587662+Gui-dsr@users.noreply.github.com>
2023-04-16 20:18:03 -0300

    Darkmode

---
+++ ctd-fe3-final-base/src/context/theme-context.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext({
+  isDarkMode: false,
+  toggleTheme: () => {},
+});
+
+export const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
